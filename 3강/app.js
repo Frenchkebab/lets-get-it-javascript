32d90a5ef5ca57794e8361424fedc52fd3f7a74a
frenchkebab <tubowangin@gmail.com>
2021-08-18 22:35:28 +0900

    if문을 ||을 사용하여 중복을 제거

--- 3강/app.js
+++ 3강/app.js
@@ -8,42 +8,24 @@ let word; // 제시어
 let newWord; // 현재 단어
 
 const onClickButton = () => {
-  if (!word) {
-    // 제시어가 비어있는가? Yes
+  // 제시어가 비어있음 or 올바른 단어입력
+  if (!wordnewWord[0] === word[word.length - 1]) {
     word = newWord;
     $word.textContent = word;
+
+    // 다음 순서로
     const order = Number($order.textContent);
     if (order + 1 > number) {
       $order.textContent = 1;
     } else {
       $order.textContent = order + 1;
     }
-    // 입력창을 비우고 커서를 둔다
-    $input.value = '';
-    $input.focus();
   } else {
-    // 제시어가 비어있는가? No
-    if (newWord[0] === word[word.length - 1]) {
-      // 새로 입력된 단어가 올바른가? Yes
-      const order = Number($order.textContent);
-      if (order + 1 > number) {
-        $order.textContent = 1;
-      } else {
-        $order.textContent = order + 1;
-      }
-      word = newWord;
-      $word.textContent = word;
-      // 입력창을 비우고 커서를 둔다
-      $input.value = '';
-      $input.focus();
-    } else {
-      // 새로 입력된 단어가 올바른가? No
-      alert('잘못된 단어입니다');
-      // 입력창을 비우고 커서를 둔다
-      $input.value = '';
-      $input.focus();
-    }
+    alert('잘못된 단어입니다');
   }
+  // 입력창을 비우고 커서를 둔다
+  $input.value = '';
+  $input.focus();
 };
 
 const onInput = (event) => {
